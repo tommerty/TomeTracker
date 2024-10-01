f2ee3ff77abdffcb184c514b94881cffdab6e9c1
Tommerty <tommerty@github.com>
2024-10-01 16:04:01 +0100

    init

---
+++ app/todo/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+type Category = {
+    name: string;
+    color: string;
+};
+
+const TodoPage = () => {
+    return <div className="flex flex-col gap-3 h-full"></div>;
+};
+
+export default TodoPage;
