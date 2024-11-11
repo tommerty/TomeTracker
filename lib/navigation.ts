364f42d915fd34b0dda896b5b071ed4bdf485ad5
Tommerty <tommerty@github.com>
2024-11-11 11:22:26 +0000

    sidebar with state

---
+++ lib/navigation.ts
@@ -0,0 +1,20 @@
+import * as tabler from "@tabler/icons-react";
+export const mainItems = [
+    {
+        title: "Time Tracking",
+        url: "/dashboard",
+        icon: tabler.IconClock,
+    },
+    {
+        title: "Todo",
+        url: "/todo",
+        icon: tabler.IconChecklist,
+    },
+];
+export const footerItems = [
+    {
+        title: "GitHub",
+        url: "https://github.com/tommerty/TomeTracker",
+        icon: tabler.IconBrandGithub,
+    },
+];
